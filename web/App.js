da417c2aed15f519a50099db71543804e883a8e3
makspll <makspl17@gmail.com>
2019-12-20 11:19:58 +0000

    Changed App to a class

--- web/App.js
+++ web/App.js
@@ -1,12 +1,13 @@
 
 
 
-const App = () => {
+class App extends React.Component{
+    render(){
     return React.createElement(
         "div", 
         { id: "some-id"},
         React.createElement("h1",{},"Some header 1")
-    );
-};
-
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+        );
+    }
+}
+ReactDOM.render(React.createElement(App, {}, null), document.getElementById("root"));
