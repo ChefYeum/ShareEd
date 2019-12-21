734a5dee3ee66fed7f8e13187b46f459940f03c0
makspll <makspl17@gmail.com>
2019-12-21 23:12:47 +0000

    index.js - updated mock objects

--- web/src/index.js
+++ web/src/index.js
@@ -7,6 +7,7 @@ import DriveFile from './utils/DriveFile';
 
 const testDrive= new ParsedDrive('Better Informatics Drive');
 testDrive.files = [
+    new DriveFile('Root'),
     new DriveFile('Year1'),
     new DriveFile('Year2'),
     new DriveFile('Year3'),
@@ -20,13 +21,20 @@ testDrive.files = [
     new DriveFile('CS'),
     new DriveFile('SE'),
 ]
-
-testDrive.files[0].setChildren(testDrive.files[5],
+testDrive.files[0].setChildren([
+    testDrive.files[1],
+    testDrive.files[2],
+    testDrive.files[3],
+    testDrive.files[4]])
+testDrive.files[1].setChildren([
     testDrive.files[6],
     testDrive.files[7],
-    testDrive.files[8]);
-testDrive.files[1].setChildren(testDrive.files[9],
+    testDrive.files[8],
+    testDrive.files[9]]);
+testDrive.files[2].setChildren([
     testDrive.files[10],
-    testDrive.files[11]);
+    testDrive.files[11],
+    testDrive.files[12]]);
+testDrive.files[6].setChildrenJSX([<h1 key ='1'>hello</h1>,<a key = '2'>link</a>])
 
     ReactDOM.render(<App currentDrive={testDrive}/>,document.getElementById('root'));
