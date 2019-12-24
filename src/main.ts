68cf7bc50cdf37485646ced547cd2a586ad3b149
ChefYeum <chefyeum@gmail.com>
2019-12-24 17:50:13 +0000

    Add dummy JSON route

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send("Yeetus"));
+app.get('/', (req, res) => res.json({
+    hippity: "hoppity"
+}));
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.listen(port, () => console.log(`Server listening on port http://localhost:${port}/`));
