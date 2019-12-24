ef1721c67682f1f5269d16d7be4e81e7b9cb14f8
ChefYeum <chefyeum@gmail.com>
2019-12-24 17:36:28 +0000

    Seup basic express server

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-console.log("what3");
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send("Yeetus"));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
