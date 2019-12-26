f0d4a8484c461cf32648721c9ba87087d6b9e385
makspll <makspl17@gmail.com>
2019-12-26 04:08:32 +0000

    updated css classes, added elements

--- web/src/components/NavBar.js
+++ web/src/components/NavBar.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import logo from '../images/favicon-32x32.png'
 
 function NavBar(){
     return(
-        <div className={'container'} >
-            <h1 className='center text-center alternate-background'>ShareEd - Google Drive Enhancer</h1>
+        <div className="navbar-div" >
+            <img className='vertical-center left' src={logo}/>
+            <h1 className='text-left no-margin-top left'><a className='no-link-effects'href='#'>ShareEd</a></h1>
+            <h1 className='text-left no-margin-top right'><a href='#'>Home</a></h1>
+            <h1 className='text-left no-margin-top right'><a href='#'>About</a></h1>
         </div>
     )
 }
