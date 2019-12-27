db7bf3f01c5e12451a9c6fc5fa6cee4740977a72
makspll <makspl17@gmail.com>
2019-12-27 16:04:12 +0000

    moved mock object

--- web/src/index.js
+++ web/src/index.js
@@ -7,42 +7,5 @@ import './css/normalize.css';
 import './css/style.css';
 import './css/customStyle.css'
 
-const testDrive= new ParsedDrive('Better Informatics Drive');
-
-testDrive.files = [
-    new DriveFile('Root'),
-    new DriveFile('Year1'),
-    new DriveFile('Year2'),
-    new DriveFile('Year3'),
-    new DriveFile('Year4'),
-    new DriveFile('Masters'),
-    new DriveFile('ILA'),
-    new DriveFile('CAP'),
-    new DriveFile('CL'),
-    new DriveFile('FP'),
-    new DriveFile('DMMR'),
-    new DriveFile('CS'),
-    new DriveFile('SE'),
-];
-
-testDrive.files[0].setChildren([
-    testDrive.files[1],
-    testDrive.files[2],
-    testDrive.files[3],
-    testDrive.files[4]]);
-
-testDrive.files[1].setChildren([
-    testDrive.files[6],
-    testDrive.files[7],
-    testDrive.files[8],
-    testDrive.files[9]]);
-
-testDrive.files[2].setChildren([
-    testDrive.files[10],
-    testDrive.files[11],
-    testDrive.files[12]]);
-
-testDrive.files[6].setChildrenJSX([<h2 key ='1'>Week1</h2>,<a href='#' key = '2'>vectors</a>,<h2 key ='3'>Week2</h2>,<a href='#' key='4'>github.com notes</a>])
-
-ReactDOM.render(<App currentDrive={testDrive}/>,document.getElementById('root'));
+ReactDOM.render(<App currentDrive={null}/>,document.getElementById('root'));
 
