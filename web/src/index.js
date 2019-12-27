05ec0fb60f9792382387173de2e6c84d62519430
makspll <makspl17@gmail.com>
2019-12-27 23:18:44 +0000

    revived mock test for producing json

--- web/src/index.js
+++ web/src/index.js
@@ -7,5 +7,44 @@ import './css/normalize.css';
 import './css/style.css';
 import './css/customStyle.css'
 
-ReactDOM.render(<App currentDrive={null}/>,document.getElementById('root'));
+const testDrive= new ParsedDrive('Better Informatics Drive');
+
+testDrive.files = [
+    new DriveFile('Root'),
+    new DriveFile('Year1'),
+    new DriveFile('Year2'),
+    new DriveFile('Year3'),
+    new DriveFile('Year4'),
+    new DriveFile('Masters'),
+    new DriveFile('ILA'),
+    new DriveFile('CAP'),
+    new DriveFile('CL'),
+    new DriveFile('FP'),
+    new DriveFile('DMMR'),
+    new DriveFile('CS'),
+    new DriveFile('SE'),
+];
+
+testDrive.files[0].setChildren([
+    testDrive.files[1].id,
+    testDrive.files[2].id,
+    testDrive.files[3].id,
+    testDrive.files[4].id]);
+
+testDrive.files[1].setChildren([
+    testDrive.files[6].id,
+    testDrive.files[7].id,
+    testDrive.files[8].id,
+    testDrive.files[9].id]);
+
+testDrive.files[2].setChildren([
+    testDrive.files[10].id,
+    testDrive.files[11].id,
+    testDrive.files[12].id]);
+
+testDrive.files[6].setChildrenJSX([<h2 key ='1'>Week1</h2>,<a href='#' key = '2'>vectors</a>,<h2 key ='3'>Week2</h2>,<a href='#' key='4'>github.com notes</a>])
+
+console.log(JSON.stringify(testDrive));
+
+ReactDOM.render(<App/>,document.getElementById('root'));
 
