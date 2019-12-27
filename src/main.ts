ce08be94a7682068fa60afe8e620cd0e16fcabdc
makspll <makspl17@gmail.com>
2019-12-27 16:03:54 +0000

    added endpoint q/drive

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 const port = 4000;
@@ -8,4 +10,9 @@ app.get('/api/data', (req, res) => res.json({
     })
 );
 
+app.get('/api/q/drive', 
+(req, res) => 
+    JSON.stringify(fs.readFileSync(path.join(__dirname,'..','data',req.query.name + '.json'),"utf8"))
+);
+ 
 app.listen(port, () => console.log(`Server listening on port http://localhost:${port}`));
