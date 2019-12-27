1c4d415e4e97d891420a14bef8fc42003019c923
makspll <makspl17@gmail.com>
2019-12-27 16:04:35 +0000

    reworked app wip

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react';
 import NavBar from './NavBar';
 import DriveDisplay from './DriveDisplay';
 import DocDisplay from './DocDisplay';
+import SearchForm from './SearchForm';
+import DriveBrowser from './DriveBrowser';
 
 class App extends Component {
     state = {
         currentDrive: this.props.currentDrive,
+        //0 - display browser, 1 - display selected drive
+        stateCode: 0,
+    }
 
+    handleSubmit(event){
+        
     }
 
     render() {
         return (
             <div>
                 <NavBar></NavBar>
-                <DriveDisplay drive={this.state.currentDrive}/>
+                <DriveBrowser/>
             </div>
         );
     }
