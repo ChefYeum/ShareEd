2c14f3f87f1d7d6277804fd5d469351de99e2a4a
makspll <makspl17@gmail.com>
2019-12-29 13:55:23 +0000

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
+import { getClient } from './mongo';
+
+//load env variables;
+require('dotenv').config({path: path.join(__dirname,"..","variables.env")});
+//connect to mongo
+const MongoClient = getClient();
+let db = null;
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.get('/api/data', (req, res) => res.json({
         hippity: "hoppity"
@@ -14,11 +21,24 @@ app.get('/api/data', (req, res) => res.json({
 app.get('/api/q/drive', 
 (req, res) => 
     {
-      const file = fs.readFileSync(path.join(__dirname,'..','data',req.query.name + '.json'),"utf8");
-      const json = JSON.parse(file);
-      res.json([json]);
-
+      db.collection('Drives').find().toArray((err,results)=>{
+        const json = JSON.parse(results);
+        res.json(json);
+      });
     }
 );
- 
-app.listen(port, () => console.log(`Server listening on port http://localhost:${port}`));
+
+app.put('/api/overwrite/drive', (req,res)=>{
+  db.collection('Drives').save(req.body, (err, result) =>{
+    if(err) return console.log(err);
+
+    console.log('saved to database');
+    res.redirect('/');
+  });
+})
+
+MongoClient.connect((err) =>{
+  if (err) return console.log(err);
+  db = MongoClient.db('ShareEdData');
+  app.listen(port, () => console.log(`Server listening on port http://localhost:${port}`));
+})
