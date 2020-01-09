4b6fd256f75266618897e6ec90881462e4cb741c
ChefYeum <chefyeum@gmail.com>
2020-01-09 14:11:28 +0000

    Google API OAuth2 setup

--- src/googleapi.ts
+++ src/googleapi.ts
@@ -1,20 +1,86 @@
-import { google } from 'googleapis';
+// Sample code from: https://developers.google.com/drive/api/v3/quickstart/nodejs
 
-import { config } from 'dotenv';
-import { resolve } from 'path';
+const fs = require('fs');
+const readline = require('readline');
+const { google } = require('googleapis');
 
-config({path: resolve(__dirname, '../.env')});
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = '../token.json';
 
-const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
-const DUMMY_FILE_ID = "1OqglQWvFyZtkYBVXrhsFYn-rDcQBLXbnpiF2e191bBU"
 
-const drive = google.drive({version: "v3", auth: GOOGLE_API_KEY});
 
-function getRes(){
-    return drive.files.export({
-        fileId: DUMMY_FILE_ID,
-        mimeType: "text/html" 
-    })
+// Load client secrets from a local file.
+fs.readFile('../credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Drive API.
+  authorize(JSON.parse(content), listFiles);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
 }
 
-getRes().then((res) => console.log(res.data))
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getAccessToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/**
+ * Lists the names and IDs of up to 10 files.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listFiles(auth) {
+  const SHAREED_DRIVE_ID = "0AEE5HGZxGrOTUk9PVA";
+  const drive = google.drive({version: 'v3', auth});
+  drive.files.list({
+      corpora: 'drive',
+      includeTeamDriveItems: true,
+      supportsTeamDrives: true,
+      teamDriveId: SHAREED_DRIVE_ID
+  }).then((res) => (console.log(res.data)));
+}
