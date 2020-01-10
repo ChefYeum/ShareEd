4176ef07e7b8305e51415edfc936301b25d456b6
ChefYeum <chefyeum@gmail.com>
2020-01-10 08:51:36 +0000

    Google API Service-to-service authentication setup

--- src/googleapi.ts
+++ src/googleapi.ts
@@ -1,86 +1,47 @@
-// Sample code from: https://developers.google.com/drive/api/v3/quickstart/nodejs
+'use strict';
 
-const fs = require('fs');
-const readline = require('readline');
-const { google } = require('googleapis');
-
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/drive'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = '../token.json';
-
-
-
-// Load client secrets from a local file.
-fs.readFile('../credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Drive API.
-  authorize(JSON.parse(content), listFiles);
-});
+import { google } from 'googleapis';
 
 /**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
+ * The JWT authorization is ideal for performing server-to-server
+ * communication without asking for user consent.
+ *
+ * Suggested reading for Admin SDK users using service accounts:
+ * https://developers.google.com/admin-sdk/directory/v1/guides/delegation
+ *
+ * See the defaultauth.js sample for an alternate way of fetching compute credentials.
  */
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
+async function runSample() {
+  // Create a new JWT client using the key file downloaded from the Google Developer Console
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "../credentials.json",
+    scopes: 'https://www.googleapis.com/auth/drive',
+  });
+  const client = await auth.getClient();
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
+  // Obtain a new drive client, making sure you pass along the auth client
+  const drive = google.drive({
+    version: 'v2',
+    auth: client,
   });
-}
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getAccessToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
-}
-
-/**
- * Lists the names and IDs of up to 10 files.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function listFiles(auth) {
+  // Make an authorized request to list Drive files.
   const SHAREED_DRIVE_ID = "0AEE5HGZxGrOTUk9PVA";
-  const drive = google.drive({version: 'v3', auth});
-  drive.files.list({
-      corpora: 'drive',
-      includeTeamDriveItems: true,
-      supportsTeamDrives: true,
-      teamDriveId: SHAREED_DRIVE_ID
-  }).then((res) => (console.log(res.data)));
+  const res = await drive.files.list({
+                corpora: 'drive',
+                includeTeamDriveItems: true,
+                supportsTeamDrives: true,
+                teamDriveId: SHAREED_DRIVE_ID
+            });
+
+  return res.data.items;
 }
+
+if (module === require.main) {
+  runSample()
+    .then(console.log)
+    .catch(console.error);
+}
+
+// Exports for unit testing purposes
+module.exports = {runSample};
